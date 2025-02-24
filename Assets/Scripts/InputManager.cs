c9871380eb58a5be17f02e962104dad39fbca475
AM <alicia.mcnett@gmail.com>
2025-02-24 12:12:00 -0500

    Starting Files

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+
+    public Text storyText; // the story 
+    public InputField userInput; // the input field object
+    public Text inputText; // part of the input field where user enters response
+    public Text placeHolderText; // part of the input field for initial placeholder text
+    
+    private string story; // holds the story to display
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        story = storyText.text;
+    }
+
+    public void UpdateStory(string msg)
+    {
+        story += "\n" + msg;
+        storyText.text = story;
+    }
+}
